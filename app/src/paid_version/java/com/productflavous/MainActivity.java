695da3b633e220c4c60b8a7d25b27f31473ff757
Harinath <harinathreddy.y1@gmail.com>
2022-07-02 21:07:15 +0530

    Poduct Flavous Initial Commit

---
+++ app/src/paid_version/java/com/productflavous/MainActivity.java
@@ -0,0 +1,26 @@
+package com.productflavous;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.productflavous.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv=(TextView) findViewById(R.id.tv);
+        tv.setText("Paid : "+BuildConfig.BaseURL);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SecondActivity.class));
+            }
+        });
+    }
+}
